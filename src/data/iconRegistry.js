844bf31545546864d11fba3b86d667efdf1be262
mano-coder <bis.anouar5@gmail.com>
2026-03-04 05:28:47 +0000

    inital Profiles data

---
+++ src/data/iconRegistry.js
@@ -0,0 +1,9 @@
+import { Dumbbell, Droplet, Flame, Utensils } from "lucide-react";
+
+// The lookup table
+export const ICON_MAP = {
+  dumbbell: Dumbbell,
+  droplet: Droplet,
+  flame: Flame,
+  utensils: Utensils,
+};
