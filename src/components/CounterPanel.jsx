9a902729730df056c1e8e203d488a687ec0b106f
mano-coder <bis.anouar5@gmail.com>
2026-03-07 05:20:19 +0000

    style: counterPanel

--- src/components/CounterPanel.jsx
+++ src/components/CounterPanel.jsx
@@ -2,7 +2,7 @@ import { Minus, Plus, RotateCcw, History, Share2 } from "lucide-react";
 
 export default function CounterPanel({ activeProfile }) {
   return (
-    <div className="text-text-primary bg-bg-elevated flex flex-1 flex-col justify-between">
+    <div className="text-text-primary bg-bg-main flex flex-1 flex-col">
       <div className="flex items-center justify-between px-8 pt-8">
         <div>
           <h1 className="text-text-primary text-2xl font-bold">
@@ -13,60 +13,70 @@ export default function CounterPanel({ activeProfile }) {
           </p>
         </div>
         <div className="flex gap-2">
-          <button className="bg-bg-card text-text-muted hover:text-text-primary rounded-xl p-2">
-            <History />
+          <button className="bg-bg-card text-text-secondary hover:text-text-primary border-stroke-card flex h-[40px] w-[40px] items-center justify-center rounded-xl border-2 border-solid">
+            <History size={20} />
           </button>
-          <button className="bg-bg-card text-text-muted hover:text-text-primary rounded-xl p-2">
-            <Share2 />
+          <button className="bg-bg-card text-text-secondary hover:text-text-primary border-stroke-card flex h-[40px] w-[40px] items-center justify-center rounded-xl border-2 border-solid">
+            <Share2 size={20} />
           </button>
         </div>
       </div>
 
-      <div className="flex items-center justify-center">
-        <span
-          className="text-text-primary font-extrabold"
-          style={{
-            fontSize: "10rem",
-            textShadow: `
+      <div className="flex flex-col items-center justify-around">
+        <div className="m-5 flex items-center justify-center">
+          <span
+            className="text-text-primary leading-none font-black"
+            style={{
+              fontSize: "14rem",
+              textShadow: `
               0 0 40px rgba(167,139,250,0.8),
               0 0 80px rgba(124,58,237,0.5),
               0 0 120px rgba(124,58,237,0.3)`,
-          }}
-        >
-          {" "}
-          {activeProfile.count}{" "}
-        </span>
-      </div>
+            }}
+          >
+            {activeProfile.count}
+          </span>
+        </div>
 
-    <div className="flex gap-4 mx-auto">
+        <div className="mx-auto flex items-center justify-center gap-6">
+          <div className="flex flex-col items-center gap-2">
+            <button className="text-text-secondary hoverbg-bg-elevated border-stroke-card flex h-[64px] w-[64px] items-center justify-center rounded-2xl border border-2 bg-[#1E293B80]">
+              <Minus size={26} strokeWidth={3} />
+            </button>
+            <span className="text-text-muted text-[10px] font-bold tracking-widest">
+              DECREASE
+            </span>
+          </div>
 
-      <div className="flex items-center gap-2">
-        <button className="bg-bg-card text-text primary hoverbg-bg-elevated rounded-2xl p-5">
-          <Minus size={20} />
-        </button>
-        <span className="text-text-muted text-[10px] tracking-widest">
-          DECREASE
-        </span>
-      </div>
+          <div className="flex flex-col items-center gap-2">
+            <button
+              style={{
+                boxShadow: "0px 0px 25px #7311D450",
+              }}
+              className="bg-accent-primary text-text-primary hover:bg-accent-bright flex h-[96px] w-[96px] items-center justify-center rounded-[24px] border-4 border-solid border-[#ffffff30]"
+            >
+              <Plus size={45} strokeWidth={3} />
+            </button>
+            <span
+              className="text-accent-bright text-[10px] font-extrabold tracking-widest"
+              style={{
+                boxShadow: "0px 0px 10px #7311D450",
+              }}
+            >
+              ADD REP
+            </span>
+          </div>
 
-      <div className="flex items-center gap-2">
-        <button className="bg-accent-primary text-text-primary hover:bg-accent-bright rounded-2xl p-6">
-          <Plus size={24} />
-        </button>
-        <span className="text-accent-bright text-[10px] tracking-widest">
-          ADD REP
-        </span>
+          <div className="flex flex-col items-center gap-2">
+            <button className="text-text-secondary hoverbg-bg-elevated border-stroke-card flex h-[64px] w-[64px] items-center justify-center rounded-2xl border border-2 bg-[#1E293B80]">
+              <RotateCcw size={24} strokeWidth={3} />
+            </button>
+            <span className="text-text-muted text-[10px] font-bold tracking-widest">
+              REST
+            </span>
+          </div>
+        </div>
       </div>
-
-      <div className="flex items-center gap-2">
-        <button className="bg-bg-card text-text-primary hoverbg-bg-elevated rounded-2xl p-5">
-          <RotateCcw size={20} />
-        </button>
-        <span className="text-text-muted text-[10px] tracking-widest">
-          REST
-        </span>
-      </div>
-    </div>
     </div>
   );
 }
