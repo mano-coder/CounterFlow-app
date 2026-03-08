99be3c2a059ee4fcce6422ea763cf63e4f8c2f0c
mano-coder <bis.anouar5@gmail.com>
2026-03-08 04:13:59 +0000

    style: Completed Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,10 @@
 import logo from "../assets/images/logo.svg";
 import { ICON_MAP } from "../data/iconRegistry";
-import { CircleDashed } from "lucide-react";
+import { CircleDashed, Settings } from "lucide-react";
 
 export default function Sidebar({ profiles, activeId, onSelectProfile }) {
   return (
-    <aside className="bg-bg-base flex h-full w-[270px] flex-col justify-between border-r border-white/5">
+    <aside className="flex h-full w-[270px] flex-col justify-between border-r border-violet-800/7 bg-[#20132F] p-3">
       <img src={logo} alt="CounterFlow logo" className="mt-4" />
 
       <nav className="flex-1 overflow-y-auto px-2 py-4">
@@ -20,39 +20,53 @@ export default function Sidebar({ profiles, activeId, onSelectProfile }) {
             <div
               key={profile.id}
               onClick={() => onSelectProfile(profile.id)}
-              className={`mb-1 flex cursor-pointer items-center gap-3 rounded-xl px-3 py-2 ${isActive ? "bg-accent-primary text-text-primary" : "text-text-secondary hover:bg-bg-card"}`}
+              className={`mb-1 flex cursor-pointer items-center gap-3 rounded-xl px-3 py-3 ${isActive ? "text-text-primary border border-purple-900 bg-[#311351]" : "text-text-secondary hover:bg-bg-card"}`}
             >
-              <Icon size={16} />
-              <span className="text-sm font-medium">{profile.name}</span>
+              <Icon
+                size={20}
+                className={`${isActive ? "text-accent-primary" : ""}`}
+                strokeWidth={3}
+              />
+              <span className="text-base font-medium">{profile.name}</span>
               {isActive && (
-                <div className="bg-accent-glow ml-auto h-2 w-2 rounded-full"></div>
+                <div className="bg-accent-primary ml-auto h-2 w-2 rounded-full"></div>
               )}
             </div>
           );
         })}
       </nav>
       <div className="mb-4 px-2">
-        <div className="mb-2 flex justify-between">
-          <span>Weekly Goal</span>
-          <span>65% Reached</span>
-        </div>
-        <div className="bg-progress-track h-1.5 w-full rounded-full">
-          <div
-            className="bg-progress-fill h-1.5 rounded-full transition-all"
-            style={{ width: "65%" }}
-          />
-        </div>
-        <div className="flex items-center gap-3">
-          <div className="bg-bg-card h-8 w-8 rounded-full" />
-          {/* avatar placeholder */}
-          <div className="flex flex-col">
-            <span className="text-text-primary text-sm font-semibold">
-              Alex Rivers
-            </span>
-            <span className="text-text-muted text-[10px] tracking-wide">
-              PRO ACCOUNT
-            </span>
+        <div className="text-text-primary rounded-xl bg-[#28133F] p-4">
+          <div className="text-text-secondary mb-2 flex justify-between">
+            <span className="text-sm">Weekly Goal</span>
           </div>
+          <div className="bg-progress-track my-2 h-1.5 w-full rounded-full">
+            <div
+              className="bg-progress-fill h-1.5 rounded-full transition-all"
+              style={{ width: "65%" }}
+            />
+          </div>
+          <span className="text-progress-fill flex justify-end text-xs font-semibold">
+            65% Reached
+          </span>
+        </div>
+        <div className="mt-3 flex items-center justify-between">
+          <div className="flex items-center gap-3">
+            <div className="bg-bg-card flex h-8 w-8 justify-between rounded-full" />
+            {/* avatar placeholder */}
+            <div className="flex flex-col">
+              <span className="text-text-primary text-sm font-semibold">
+                Alex Rivers
+              </span>
+              <span className="text-text-muted text-[10px] font-semibold tracking-wide">
+                PRO ACCOUNT
+              </span>
+            </div>
+          </div>
+          <Settings
+            className="text-text-muted mr-1 flex justify-end"
+            size={15}
+          />
         </div>
       </div>
     </aside>
