8cd42a45d9c519886d308ac20e784dda1c71013b
mano-coder <bis.anouar5@gmail.com>
2026-03-11 06:07:47 +0000

    feat: add shortcuts for confirm and cancel button in ConfirmationDiaglog

--- src/hooks/useKeyboard.js
+++ src/hooks/useKeyboard.js
@@ -1,20 +1,53 @@
 import { useHotkeys } from "react-hotkeys-hook";
 
-export function useKeyboard({ increase, decrease, reset, tab }) {
-  useHotkeys("ArrowUp", (e) => {
-    e.preventDefault();
-    increase();
-  });
-
-  useHotkeys("ArrowDown", (e) => {
-    e.preventDefault();
-    decrease();
-  });
-
-  useHotkeys("r", reset);
-
-  useHotkeys("tab", (e) => {
-    e.preventDefault();
-    tab();
-  });
+export function useKeyboard({
+  increase,
+  decrease,
+  reset,
+  tab,
+  isModalOpen,
+  closeModal,
+  confirm,
+}) {
+  useHotkeys(
+    "ArrowUp",
+    (e) => {
+      e.preventDefault();
+      increase();
+    },
+    { enabled: !isModalOpen },
+  );
+  useHotkeys(
+    "ArrowDown",
+    (e) => {
+      e.preventDefault();
+      decrease();
+    },
+    { enabled: !isModalOpen },
+  );
+  useHotkeys("r", reset, { enabled: !isModalOpen });
+  useHotkeys(
+    "tab",
+    (e) => {
+      e.preventDefault();
+      tab();
+    },
+    { enabled: !isModalOpen },
+  );
+  useHotkeys(
+    "escape",
+    (e) => {
+      e.preventDefault();
+      closeModal();
+    },
+    { enabled: isModalOpen },
+  );
+  useHotkeys(
+    "enter",
+    (e) => {
+      e.preventDefault();
+      confirm();
+    },
+    { enabled: isModalOpen },
+  );
 }
