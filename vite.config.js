e363a252c1a6331753661b1750bf368cd0cc49e7
mano-coder <bis.anouar5@gmail.com>
2026-05-17 14:31:26 +0100

    add slashes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'CounterFlow-app',
+  base: '/CounterFlow-app/',
   plugins: [react(), tailwindcss()],
   build: {
     sourcemap: true
